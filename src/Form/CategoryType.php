1addb6c1292068f361652f9b6430b66f314f4fb9
R_michel <isdalon1@gmail.com>
2021-02-04 10:55:59 +0100

    first commit

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -2,30 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class ArticleType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('category', EntityType::class, [
-                'class' => 'App\Entity\Category',
-                'choice_label' => 'name'
-            ])
+            ->add('name')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Article::class,
+            'data_class' => Category::class,
         ]);
     }
 }
